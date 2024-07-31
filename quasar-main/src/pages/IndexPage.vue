3a1ab2241f974e8ad9fd81366f1e280adbf19f23
phngskrkhawang@gmail.com <>
2024-07-31 10:48:08 +0700

    modified id-code

--- quasar-main/src/pages/IndexPage.vue
+++ quasar-main/src/pages/IndexPage.vue
@@ -5,7 +5,15 @@
       @submit="onSubmit"
       @reset="onReset"
       class="q-gutter-md"
-    >
+      >
+      <q-input
+        filled
+        v-model="id"
+        label="Your id *"
+        hint="id-code "
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your id-code']"
+        />
       <q-input
         filled
         v-model="name"
@@ -53,13 +61,14 @@ import { ref } from 'vue'
 export default {
   setup () {
     const $q = useQuasar()
-
+    const id = ref("6604101352")
     const name = ref("พงศกร")
     const surname = ref("คำวัง")
     const age = ref(null)
     const accept = ref(false)
 
     return {
+      id,
       name,
       surname,
       age,
